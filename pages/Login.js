ec3b159d5777270fded4a61004c525b5c2d3b55e
11134212 <chilikenewbekov@gmail.com>
2023-01-10 16:11:37 +0300

    part2

---
+++ pages/Login.js
@@ -0,0 +1,29 @@
+import { getProviders, signIn } from 'next-auth/react'
+function Login({ providers }) {
+	return (
+		<div>
+			<img
+				src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png'
+				className='w-52 mb-5'
+			/>
+			{Object.values(providers).map(provider => (
+				<div key={provider.name}>
+					<button onClick={provider.id}>Login with {provider.name}</button>
+				</div>
+			))}
+		</div>
+	)
+}
+console.log(Login)
+
+export default Login
+
+export async function getServerSideProps() {
+	const providers = await getProviders()
+
+	return {
+		props: {
+			providers,
+		},
+	}
+}
